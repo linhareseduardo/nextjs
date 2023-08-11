7afdf41b3d3d796877e2b4916167d6df0e0ae476
linhareseduardo <linhareseduardo7@gmail.com>
2023-08-11 15:13:06 -0300

    aula 10

--- aula09/src/routes.js
+++ aula09/src/routes.js
@@ -3,6 +3,9 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Contato from './pages/Contato';
+import Erro from './pages/Erro';
+import Produto from './pages/Produto';
+
 
 import Header from './components/Header';
 
@@ -13,7 +16,10 @@ const Routes = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/sobre" component={Sobre} />
-        <Route path="/contato" component={Contato} />
+        <Route exact path="/contato" component={Contato} />
+        <Route path="/produto/:id" component={Produto} />
+
+        <Route path="*" component={Erro} />
       </Switch>
     </BrowserRouter>
   )
